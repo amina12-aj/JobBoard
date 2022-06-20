016b95c4de7921f030f2f546eb8ad3b5122f9a40
amina12-aj <bakareaminata@gmail.com>
2022-06-20 18:27:07 +0100

    Main

---
+++ numbers.js
@@ -0,0 +1,7 @@
+$(document).ready(function()){
+    var value = 
+$(this).val().tolowercase();
+    $().filter(function()) {
+        $(this).toggle($(this).text().lowercase().indexof(value) > -1)})
+    })
+})
