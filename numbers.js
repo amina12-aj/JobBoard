6fc8f77e41955ec1989cc341efceefb8413679db
amina12-aj <bakareaminata@gmail.com>
2022-06-20 19:26:33 +0100

    main

--- numbers.js
+++ numbers.js
@@ -1,13 +1,3 @@
 
-$("#searchTheKey").on('keyup', function(){
-    var value = $(this).val().toLowerCase();
-    $("#network", "#operations", "#sales","#sharedss", "#software").each(function () {
-       if ($(this).text().toLowerCase().search(value) > -1) {
-          $(this).show();
-          $(this).prev('.subjectName').last().show();
-       } else {
-          $(this).hide();
-       }
-    });
- })
+
 
